b3077a654327fbdeccc13a20cf537807ae0e2293
fun and creative username <hiro@wintermute.localdomain>
2019-10-25 00:05:59 -0400

    added basic functionality for client interaction

--- talk_client.py
+++ talk_client.py
@@ -2,11 +2,14 @@ import select
 from socket import *
 import sys
 import argparse
+from _thread import *
+import threading
 
+def prRed(skk): print("\033[91m {}\033[00m" .format(skk)) 
 
 parser = argparse.ArgumentParser(description="A prattle client")
 
-parser.add_argument("-n", "--name", dest="name", help="name to be prepended in messages (default: machine name)")
+parser.add_argument("-n", "--name", dest="name", type=str, default="machine name", help="name to be prepended in messages (default: machine name)")
 parser.add_argument("-s", "--server", dest="server", default="127.0.0.1",
                     help="server hostname or IP address (default: 127.0.0.1)")
 parser.add_argument("-p", "--port", dest="port", type=int, default=12345,
@@ -14,3 +17,51 @@ parser.add_argument("-p", "--port", dest="port", type=int, default=12345,
 parser.add_argument("-v", "--verbose", action="store_true", dest="verbose",
                     help="turn verbose output on")
 args = parser.parse_args()
+
+def runProgram():
+
+    port = args.port
+    verbose = args.verbose
+    
+    #socket initialization found at https://steelkiwi.com/blog/working-tcp-sockets/
+    sock = socket(AF_INET, SOCK_STREAM)
+
+    while True:
+        try:
+            sock.connect((args.server, args.port))
+            sock.setblocking(0)
+            break
+        except:
+            prRed("connection not established.")
+            confirmation = input("       Try again? (y/n): ")
+
+            if(confirmation == "n" or confirmation == "N"):
+                return   
+
+    start_new_thread(getMessages, (sock,))
+    sendMessages(sock)
+    sock.close()
+
+        
+def sendMessages(sock):
+
+    name = args.name
+
+    while True:
+        incoming, outcoming, uhoh = select.select([sock], [sock], [sock], 1)
+
+        userMessage = input()
+        #userMessage += args.name
+        userMessage = name + " says:" + userMessage
+        sock.send(userMessage.encode())
+
+def getMessages(sock):
+    while True:
+        incoming, outcoming, uhoh = select.select([sock], [sock], [sock], 1)
+        try:
+            print(sock.recv(1024).decode())
+        except:
+            yes = True
+
+if __name__ == "__main__":
+    runProgram()
